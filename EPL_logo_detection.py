c44a5f4dd5065d0467818daf32de050200506111
Jobin nelson <jobinnelson369@gmail.com>
2022-02-06 21:53:13 +0530

    first commit

---
+++ EPL_logo_detection.py
@@ -0,0 +1,6 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def create_generators(batch_size, train_path, val_path, test_path):
+    preprocessor = ImageDataGenerator(
+        rescale=
+    )
