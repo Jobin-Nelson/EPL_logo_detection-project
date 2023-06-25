473dd561f59a7b9e48ae909a27b70eabb655836f
Jobin Nelson <jobinnelson369@gmail.com>
2023-06-25 16:48:34 +0530

    convert strings to path

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import predictor
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -11,11 +12,11 @@ def home():
 def submit():
     if request.method == 'POST':
         img = request.files['Image']
-        img_path = 'static/' + img.filename
+        img_path = Path(__file__).parent / 'static' / img.filename
         img.save(img_path)
         label = predictor.predict_with_model(img_path)
         return render_template('sub.html', label=label, img_path=img_path)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
